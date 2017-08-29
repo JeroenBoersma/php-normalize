faabe585cb5af42abaeed54f8cd71efc062927a3
Jeroen Boersma <jeroen@srcode.nl>
2017-08-29 08:03:33 +0000

    Added a Safe rule
    + safe rule, used to run until no changes occur
      useful for anti remote file inclusion

---
+++ src/Rule/Safe.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @copyright (c) 2017 Srcode
+ */
+
+declare(strict_types=1);
+
+namespace Srcoder\Normalize\Rule;
+
+class Safe implements RuleInterface
+{
+
+    /** @var RuleInterface */
+    protected $rule;
+    /** @var int */
+    protected $limit;
+
+    public function __construct(RuleInterface $rule, int $limit = -1)
+    {
+        $this->rule = $rule;
+        $this->limit = $limit;
+    }
+
+    /**
+     *
+     * @param string $string
+     * @return string
+     */
+    public function apply(string $string) : string
+    {
+        while (($result = $this->rule->apply($string)) != $string && ($limit === -1 || $limit-- > -1)) {
+            $string = $result;
+        }
+
+        return $result;
+    }
+
+}
